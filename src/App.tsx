79ad18bc103e242c4e01a29c955a5b78c7d80295
Shrujan <shrork@gmail.com>
2023-04-01 20:00:08 +0530

    reverted tp basic template

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
-import UserForm from './components/UserFormComponent/user-form';
-import { UserCardContainer } from './components/UserCardContainer/user-card-coontainer';
-import { UsersContainer } from './components/UsersContainer/users-container';
 
 function App() {
   return (
     <div className="App">
-      <UsersContainer></UsersContainer>
+      Basic App
     </div>
   );
 }
--- src/components/UserCard/user-card.component.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './user-card.module.scss'
-
-export const UserCard = (props: { userName: string, userAge: number}) => {
-  return (
-    <div className={ styles['user-card'] }>
-      { props.userName } ({ props.userAge } years old)
-    </div>
-  )
-}
