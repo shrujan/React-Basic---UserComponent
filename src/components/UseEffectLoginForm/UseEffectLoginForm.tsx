19add8085a2a529147333677ee48fe23a285a0e0
Shrujan <shrork@gmail.com>
2023-04-15 10:41:31 +0530

    cleanup

--- src/components/UseEffectLoginForm/UseEffectLoginForm.tsx
+++ src/components/UseEffectLoginForm/UseEffectLoginForm.tsx
@@ -4,18 +4,20 @@ export const UseEffectLoginForm = () => {
 
   const [ userName, setUserName ] = useState('');
   const [ password, setPassword ] = useState('');
+  const [ isValidForm, setFormValididity ] = useState(false);
 
   useEffect(() => {
-    const validateForm = () => {
-      console.log(userName, password)
-    }
+    // will get triggered and executed when username or password changes
+    setFormValididity(
+      (userName.trim().length > 0 && password.trim().length > 3)
+    )
   }, [ userName, password ])
 
   return <section>
     <form>
       <input type="text" name="userName" onKeyUp={ (e: any) => { setUserName(e.target.value) }  }></input>
       <input type="password" name="password" onKeyUp={ (e: any) => { setPassword(e.target.value) } }></input>
-      <button type="submit">Login</button>
+      <button type="submit" disabled={ !isValidForm }>Login</button>
     </form>
   </section>
 }
