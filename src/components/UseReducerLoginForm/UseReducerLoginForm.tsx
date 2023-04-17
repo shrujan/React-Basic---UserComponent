ff3aedd0f0019ca987f645a4e0e31704a46115ab
Shrujan <shrork@gmail.com>
2023-04-17 16:56:30 +0530

    reducer

--- src/components/UseReducerLoginForm/UseReducerLoginForm.tsx
+++ src/components/UseReducerLoginForm/UseReducerLoginForm.tsx
@@ -1,12 +1,32 @@
-import { useState } from "react"
+import { useReducer, useState } from "react";
+
+const userNameReducer = (state: any, action: any) => {
+  // this is outside the component function as it does not need any data from within the compoenent -
+  // All the data needed will be passed as params
+  console.log(state, action)
+  if (action.type === 'USER_NAME') {
+    return { value: action.value, isValid: action.value.length > 1 }
+  }
+  if (action.type === "USER_NAME_BLUR") {
+    // state.value will always be the latest state
+    return { value: state.value, isValid: state.value.length > 1 }
+  }
+  return { value: "", isValid: false }
+}
 
 // convert Basic Login form to use reducer to combine states
 export const UseReducerLoginForm = () => {
   // use Usereducer when we have a scenario where one state depends on another state 
   // in the below scenario userName and isUserNameValid are interlinked similarly for password and isPasswordValid
 
-  const [ userName, setUserName ]           = useState('');
-  const [ isUserNameValid, setIsValidUser ] = useState(false);
+  // const [ userName, setUserName ]           = useState('');
+  // const [ isUserNameValid, setIsValidUser ] = useState(false);
+
+  // reducer
+  const [ userNameState, userNameDispatcher ] = useReducer(userNameReducer, {
+    value: "",
+    isValid: false
+  })
 
   const [ password, setPassword ]               = useState('');
   const [ isPasswordValid, setIsPasswordValid ] = useState(false);
@@ -14,7 +34,12 @@ export const UseReducerLoginForm = () => {
   const [ isValidForm, setFormValididity ] = useState(false);
 
   const handleUserNameState = (event: any) => {
-    setUserName(event.target.value);
+    // setUserName(event.target.value);
+    userNameDispatcher({
+      type: "USER_NAME",
+      value: event.target.value
+    })
+
     // setIsValidUser((event.target.value).length > 0)
     setFormValididity(
       (event.target.value).length > 0 && password.length > 3
@@ -25,12 +50,15 @@ export const UseReducerLoginForm = () => {
     setPassword(event.target.value);
     // setIsPasswordValid((event.target.value).length > 3);
     setFormValididity(
-      userName.length > 0 && (event.target.value).length > 3
+      userNameState.value.length > 0 && (event.target.value).length > 3
     )
   }
 
   const validateUserNameHandler = () => {
-    setIsValidUser(userName.length > 1);
+    // setIsValidUser(userNameState.value.length > 1);
+    userNameDispatcher({
+      type: "USER_NAME_BLUR" 
+    })
   };
 
   const validatePasswordHandler = () => {
@@ -49,7 +77,7 @@ export const UseReducerLoginForm = () => {
         <input
           id="userName"
           type="text"
-          value={ userName }
+          value={ userNameState.value }
           onChange={ (e: any) => { handleUserNameState(e) } }
           onBlur={ validateUserNameHandler }
         ></input>
@@ -63,7 +91,6 @@ export const UseReducerLoginForm = () => {
           onBlur={ validatePasswordHandler }
         ></input>
       </div>
-      
       <button type="submit" disabled={ !isValidForm }>Login</button>
     </form>
   </section>
