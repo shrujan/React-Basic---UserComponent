f60672a970ea1dba9c067a8607a7d1624f8f97d5
Shrujan <shrork@gmail.com>
2023-04-24 12:13:01 +0530

    added new menu

--- src/components/Exercice-2-React-Menu/Cart/Cart.tsx
+++ src/components/Exercice-2-React-Menu/Cart/Cart.tsx
@@ -2,7 +2,9 @@ import styles from './Cart.module.scss';
 import { CartIcon } from './CartIcon';
 
 export const Cart = () => {
-  return <section>
+  return <section className={ styles['cart'] }>
     <CartIcon />
+    <label>Your Cart</label>
+    <span>5</span>
   </section>
 }
