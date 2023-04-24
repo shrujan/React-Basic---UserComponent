559c2ab16a6211e39f357a7ab367298a5e5d36e5
Shrujan <shrork@gmail.com>
2023-04-24 16:48:51 +0530

    home regiged

--- src/components/Exercice-2-React-Menu/Home.tsx
+++ src/components/Exercice-2-React-Menu/Home.tsx
@@ -1,8 +1,12 @@
 import { Header } from "./Header/Header";
 import style from './Home.module.scss'
+import { Intro } from "./Intro/Intro";
 
 export const MenuHome = () => {
   return <main className={ style['menu-main'] }>
     <Header></Header>
+    <section>
+      <Intro></Intro>
+    </section>
   </main>
 }
---
+++ src/components/Exercice-2-React-Menu/Intro/Intro.tsx
@@ -0,0 +1,9 @@
+import styles from './Intro.module.scss'
+
+export const Intro = () => {
+  return (
+    <div className={ styles['intro'] }>
+      <p>This is the basic Intro</p>
+    </div>
+  )
+}
