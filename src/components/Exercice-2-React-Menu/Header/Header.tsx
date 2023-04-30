97b914f97483865ffff4c94794d3fd1a9f1abe60
Shrujan <shrork@gmail.com>
2023-04-30 16:26:27 +0530

    image added

--- src/components/Exercice-2-React-Menu/Header/Header.tsx
+++ src/components/Exercice-2-React-Menu/Header/Header.tsx
@@ -1,11 +1,18 @@
 import { Cart } from '../Cart/Cart'
-import styles from './Header.module.scss'
+import styles from './Header.module.scss';
+import mealsImg from '../Assets/meals.jpg'
 
 export const Header = () => {
   return (
-    <header className={ styles['header'] }>
-      <h3>React Meals</h3>
-      <Cart></Cart>
-    </header>
+    <>
+      <header className={ styles['header'] }>
+        <h3>React Meals</h3>
+        <Cart></Cart>
+      </header>
+      <div className={ styles['main-image'] }>
+        <img src={ mealsImg } />
+      </div>
+    </>
+    
   )
 }
