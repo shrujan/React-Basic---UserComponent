68431ac48e857f6cf0fce1dbbd5400a58ac5b9b5
Shrujan <shrork@gmail.com>
2023-05-26 09:24:43 +0530

    added nasic form

--- src/Routing/ReactForms.tsx
+++ src/Routing/ReactForms.tsx
@@ -1,8 +1,19 @@
 import ReactFormExample from "../components/ReactFormsExample/ReactFormExample";
 
 const BasicReactForms = () => {
-  // return <ReactFormExample />
-  return <p>Forms</p> 
+
+  const submitForm = () => {}
+
+  return <section>
+    <form onSubmit={ submitForm }>
+      <div className="form-control">
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name"></input>
+      </div>
+      
+      <button>Save</button>
+    </form>
+  </section>
 }
 
 export default BasicReactForms;
