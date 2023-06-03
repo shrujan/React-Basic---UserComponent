1cd682c0b8d9b2e4a78dd68b7135878ed503ce93
Shrujan <shrork@gmail.com>
2023-06-03 17:36:11 +0530

    reducer done

--- src/components/UseReducerBasic/user-list.tsx
+++ src/components/UseReducerBasic/user-list.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import styles from './user-list.module.scss'
 
 const data = [
@@ -6,33 +6,112 @@ const data = [
   { name: 'John',    age: '32', id: 2 },
   { name: 'Mike',    age: '32', id: 3 },
   { name: 'happry',  age: '32', id: 4 },
-]
+];
+
+const CLEAR_ALL   = 'CLEAR_ALL';
+const RESET_ALL   = 'RESET_ALL';
+const DELETE_USER = 'DELETE_USER';
+
+interface User {
+  name: string,
+  age: string,
+  id: number
+}
+
+interface StateData {
+  userList: User[]
+}
+
+const defaultState = {
+  userList: data
+}
+
+
+const reducer = (state: StateData, action: any) => {
+
+  switch (action.type) {
+    case CLEAR_ALL:
+      return {
+        ...state,
+        userList: []
+      }
+    case RESET_ALL:
+      return {
+        ...state,
+        userList: data
+      }
+    case DELETE_USER:
+      const userId = action.payload.id;
+      const filtered = state.userList.filter(user => user.id !== userId);
+      return {
+        ...state,
+        userList: filtered
+      }
+    default:
+      return state
+  }
+  // return state;
+};
+
 
 const UserListBasicReducerEx = () => {
-  const [ userList, setUserList ] = useState(data);
+
+  // -------------- Normal state methods 
+
+  // const [ userList, setUserList ] = useState(data);
+
+  // const deleteUser = (id: number) => {
+  //   const filtered = userList.filter(user => user.id !== id);
+  //   setUserList(filtered);
+  // }
+
+  // const clearAllUsers = () => {
+  //   setUserList([])
+  // }
+
+  // const resetUsers = () => {
+  //   setUserList(data)
+  // }
+
+  // return <section className={ styles['user-list'] }>
+  //   { userList.map( user => (
+  //     <div key={ user.id }>
+  //       <label> { user.name } </label>
+  //       <button onClick={() => deleteUser(user.id) }>Delete</button>
+  //     </div>
+      
+  //   ) ) }
+  //   <button onClick={ userList.length > 0 ? clearAllUsers : resetUsers }>{ userList.length > 0 ? 'Clear All' : 'Reset All' }</button>
+  // </section>
+
+  // -----------------------------------
+
+  // Reducer way
+  
+  const [ state, dispatch ] = useReducer(reducer, defaultState);
 
   const deleteUser = (id: number) => {
-    const filtered = userList.filter(user => user.id !== id);
-    setUserList(filtered);
+    dispatch({ type: DELETE_USER, payload: { id } })
   }
 
   const clearAllUsers = () => {
-    setUserList([])
+    dispatch({ type: CLEAR_ALL })
   }
 
   const resetUsers = () => {
-    setUserList(data)
+    dispatch({ type: RESET_ALL })
   }
 
   return <section className={ styles['user-list'] }>
-    { userList.map( user => (
+    { state.userList.map( (user: any) => (
       <div key={ user.id }>
         <label> { user.name } </label>
-        <button onClick={() => deleteUser(user.id) }>Clear</button>
+        <button onClick={() => deleteUser(user.id) }>Delete</button>
       </div>
       
     ) ) }
-    <button onClick={ userList.length > 0 ? clearAllUsers : resetUsers }>{ userList.length > 0 ? 'Clear All' : 'Reset All' }</button>
+    <button onClick={ state.userList.length > 0 ? clearAllUsers : resetUsers }>{ state.userList.length > 0 ? 'Clear All' : 'Reset All' }</button>
+
   </section>
 }
 
