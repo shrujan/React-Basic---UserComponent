afac0ab961fe929973b1a444b7ded534d113e287
Shrujan <shrork@gmail.com>
2023-06-15 16:21:09 +0530

    created basic store

---
+++ src/store/index.tsx
@@ -0,0 +1,21 @@
+import { legacy_createStore as createStore} from 'redux';
+
+const counterReducer = (state = { counter: 0 }, action: any) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter + 1
+    }
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter - 1
+    }
+  }
+
+  return state
+}
+
+const store = createStore(counterReducer);
+
+export default store;
