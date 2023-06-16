97e6d20d86060ab660cccedf2182859cb286836d
Shrujan <shrork@gmail.com>
2023-06-16 17:26:05 +0530

    finishd dispatching actions

--- src/components/ReduxCounter/CounterDisplay.tsx
+++ src/components/ReduxCounter/CounterDisplay.tsx
@@ -1,16 +1,30 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 
 const CounterDisplay = () => {
   // useSelector() // allows us to select a part of store
   // useStore() // selects entire store
+  // useDispatch hook lets us dispatch an action from component to reducer fn to update store
   // when the component is unmounted then React reduc will clear the subscription automatically
 
   // here we extracted the counter info from store
   const counter = useSelector((state: any) => state.counter); // pass fuction to extract which data we need from store.
+  const dispatch = useDispatch();
+
+  const incrementCounter = () => {
+    dispatch({ type: 'increment' });
+  }
+  
+  const decrementCounter = () => {
+    dispatch({ type: 'decrement' });
+  }
 
   return <div>
     counter == { counter }
+    <div>
+      <button onClick={ incrementCounter }>Increment Counter</button>
+      <button onClick={ decrementCounter }>Decrement Counter</button>
+    </div>
     <button>Toggle </button>
   </div>
 }
