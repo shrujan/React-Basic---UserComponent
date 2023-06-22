eadee67a076ef8e9624d517bb8029d4f63eabf88
Shrujan <shrork@gmail.com>
2023-06-22 20:18:31 +0530

    removed comments

--- src/store/index-toolkit.tsx
+++ src/store/index-toolkit.tsx
@@ -33,37 +33,6 @@ const counterSlice = createSlice({
   }
 })
 
-// const initialState = {
-//   counter: 0
-// }
-
-// const counterReducer = (state = initialState, action: any) => {
-//   if (action.type === 'increment') {
-//     return {
-//       counter: state.counter + 1
-//     }
-//   }
-
-//   if (action.type === 'incrementBy') {
-//     return {
-//       counter: state.counter + action.count // count is the action param which will be used in controller to dispatch action with payload
-//     }
-//   }
-
-//   if (action.type === 'decrementBy') {
-//     return {
-//       counter: state.counter - action.count // count is the action param which will be used in controller to dispatch action with payload
-//     }
-//   }
-
-//   if (action.type === 'decrement') {
-//     return {
-//       counter: state.counter - 1
-//     }
-//   }
-
-//   return state
-// }
 
 // this will work - but there will be a problem as there can be only one slice reducer
 // const store = createStore(counterSlice.reducer);
@@ -73,5 +42,7 @@ const store = configureStore({
   // reducer: counterSlice.reducer // pass global state slice's reducer
   reducer: { counter: counterSlice.reducer } // we can also pass map of reducers and it will get merged into one big reducer
 })
+
+export const actions = counterSlice.actions;
 
 export default store;
