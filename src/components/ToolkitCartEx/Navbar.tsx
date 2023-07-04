36311180adc5798a5fed25172dc4be2d8588c913
Shrujan <shrork@gmail.com>
2023-07-04 17:21:51 +0530

    react cart

--- src/components/ToolkitCartEx/Navbar.tsx
+++ src/components/ToolkitCartEx/Navbar.tsx
@@ -3,10 +3,15 @@ import { useSelector } from 'react-redux'
 import styles from './Cart.module.scss'
 
 const Navbar = () => {
-  const { amount } = useSelector((state: any) => state.cart);
-  
+  const { amount } = useSelector((store: any) => store.cart);
+  console.log(amount)
   return <nav className={ styles['navbar-center'] }>
     <h3>Redux toolkit</h3>
+    <div className={ styles['nav-container'] }>
+      <div className={ styles['amount-container'] }>
+
+      </div>
+    </div>
   </nav>
 }
 
