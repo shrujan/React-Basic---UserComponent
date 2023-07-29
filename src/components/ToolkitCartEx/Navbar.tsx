8823b7f9e8cad3009b9512710fd6488bfffb406d
Shrujan <shrork@gmail.com>
2023-07-29 17:30:54 +0530

    partial cart

--- src/components/ToolkitCartEx/Navbar.tsx
+++ src/components/ToolkitCartEx/Navbar.tsx
@@ -1,6 +1,7 @@
 
 import { useSelector } from 'react-redux';
 import styles from './Cart.module.scss';
+import { CartIcon } from '../../icons/Icons';
 
 const Navbar = () => {
   const { amount } = useSelector((store: any) => store.cart);
@@ -8,8 +9,9 @@ const Navbar = () => {
   return <nav className={ styles['navbar-center'] }>
     <h3>Redux toolkit</h3>
     <div className={ styles['nav-container'] }>
+      <CartIcon />
       <div className={ styles['amount-container'] }>
-
+        0
       </div>
     </div>
   </nav>
--- src/store/cart.tsx
+++ src/store/cart.tsx
@@ -1,7 +1,26 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+// temporarily set the initial state
+
+const itemList = [
+  {
+    id: 'sdfsfd123123',
+    title: "Samsung galaxu 1",
+    price: "20000",
+    img: "",
+    amount: 1
+  }
+]
+
+interface CartState {
+  cartItems: any[],
+  amount: number,
+  total: number,
+  isLoading: boolean
+}
+
 const sliceInitialState = {
-  cartItems: [],
+  cartItems: itemList,
   amount: 0,
   total: 0,
   isLoading: true
@@ -11,16 +30,23 @@ const cartReducer = () => {
   
 }
 
-const slice = createSlice({
+const clearCart = (state: CartState) => {
+  state.cartItems = [];
+}
+
+const cartSlice = createSlice({
   name: "cart",
   initialState: sliceInitialState,
   reducers: {
-    cart: cartReducer
+    cart: cartReducer,
+    clearCart: clearCart
   }
 })
 
+console.log(cartSlice)
+
 export const store = configureStore({
   reducer: {
-    cart: slice.reducer
+    cart: cartSlice.reducer
   }
 })
