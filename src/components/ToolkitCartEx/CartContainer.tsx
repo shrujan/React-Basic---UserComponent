e4ab5016bbe1bfe4baf8b2e17864a75102a0430c
Shrujan <shrork@gmail.com>
2023-09-05 17:11:30 +0530

    changes

--- src/components/ToolkitCartEx/CartContainer.tsx
+++ src/components/ToolkitCartEx/CartContainer.tsx
@@ -5,7 +5,6 @@ import { CartItem } from "./CartItem";
 const CartContainer = () => {
    // cartItem is the parameter in store and the store is provided in parent component
    const { cartItems, amount, total } = useSelector((store: any) => store.cart)
-   console.log(cartItems)
 
    if (amount < 1) {
     return <section className={ styles['cart'] }>
@@ -16,21 +15,19 @@ const CartContainer = () => {
           <h4 className={ styles['empty-cart'] }>Your bag is empty</h4>
         </header>
       </section>
+   } else {
+    return <section className={ styles['cart'] }>
+      <header>
+        <h2>Your Bag</h2>
+      </header>
+      <div>
+        { cartItems.map((item: any) => {
+            return <CartItem key={ item.id } title={ item.title }></CartItem>
+          })
+        }
+      </div>
+    </section>
    }
-
-   return <section className={ styles['cart'] }>
-    <header>
-      <h2>Your Bag</h2>
-    </header>
-    <div>
-      { cartItems.map((item: any) => {
-         <CartItem title={ item.title }></CartItem>
-      }) }
-    </div>
-
-   </section>
-   
-
 }
 
 export default CartContainer
--- src/components/ToolkitCartEx/CartItem.tsx
+++ src/components/ToolkitCartEx/CartItem.tsx
@@ -1,8 +1,6 @@
-import { useSelector } from 'react-redux';
-import styles from './Cart.module.scss';
+// import styles from './Cart.module.scss';
 
 export const CartItem = (props: any) => {
- 
   return <section>
     { props.title }
   </section>
